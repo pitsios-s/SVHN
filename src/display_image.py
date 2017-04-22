f07c145689eff88f1a4655cb9cbcda29dc905c42
pitsios-s <stamatis.pit@gmail.com>
2017-04-22 19:02:01 +0300

    small fix in display_image.py

--- src/display_image.py
+++ src/display_image.py
@@ -1,7 +1,9 @@
 import scipy.io
 from matplotlib import pyplot as plt
 
+# Read .mat file
 mat = scipy.io.loadmat('file.mat')
 
-plt.imshow(data, interpolation='nearest')
+# Disaply first image
+plt.imshow(mat['X'][:, :, :, 0], interpolation='nearest')
 plt.show()
