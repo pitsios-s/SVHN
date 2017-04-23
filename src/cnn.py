9b58c815c4eeac040c02f4d459d57ce3daf0863f
pitsios-s <stamatis.pit@gmail.com>
2017-04-23 20:15:26 +0300

    Cleaned scripts from unnecessary code

--- src/cnn.py
+++ src/cnn.py
@@ -1,98 +1,69 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from __future__ import print_function
-
 import tensorflow as tf
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-import scipy.io
-mat = scipy.io.loadmat('file.mat') # load file
-
-
-# 
-# <img src="http://www.rsipvision.com/wp-content/uploads/2015/04/Slide6.png">
-# 
-
-# In[11]:
-
 
 # Parameters
 learning_rate = 0.001
-training_iters = 30000
+training_iters = 500000
 batch_size = 128
 display_step = 100
 
 # Network Parameters
-n_input = 1024 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
-dropout = 0.75 # Dropout, probability to keep units                  ##### play
-
-
-# In[3]:
-
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+dropout = 0.75  # Dropout, probability to keep units                  ##### play
 
 # tf Graph input
-x = tf.placeholder(tf.float32, [None, n_input])
-y = tf.placeholder(tf.float32, [None, n_classes])
-keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
-
-
-# In[4]:
+X = tf.placeholder(tf.float32, [None, n_input])
+Y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
 
 
 # Create some wrappers for simplicity
-def conv2d(x, W, b, strides=1):
+def conv2d(x, w, b, strides=1):
     # Conv2D wrapper, with bias and relu activation
-    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.conv2d(x, w, strides=[1, strides, strides, 1], padding='SAME')
     x = tf.nn.bias_add(x, b)
     return tf.nn.relu(x)
 
+
 def maxpool2d(x, k=2):
     # MaxPool2D wrapper
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')    ### other option is average pooling (or combination)
+    # other option is average pooling (or combination)
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+
 
 # Create model
-def conv_net(x, weights, biases, dropout):
+def conv_net(x, w, b, d):
     # Reshape input picture
-    x = tf.reshape(x, shape=[-1, 32, 32, 1])  ### ?
+    x = tf.reshape(x, shape=[-1, 32, 32, 1])  # ?
     #
     # Convolution Layer
-    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    conv1 = conv2d(x, w['wc1'], b['bc1'])
     # Max Pooling (down-sampling)
     conv1 = maxpool2d(conv1, k=2)
     #
     # Convolution Layer
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = conv2d(conv1, w['wc2'], b['bc2'])
     # Max Pooling (down-sampling)
     conv2 = maxpool2d(conv2, k=2)
     #
     # Fully connected layer
     # Reshape conv2 output to fit fully connected layer input
-    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.reshape(conv2, [-1, w['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, w['wd1']), b['bd1'])
     fc1 = tf.nn.relu(fc1)
     # Apply Dropout
-    fc1 = tf.nn.dropout(fc1, dropout)
+    fc1 = tf.nn.dropout(fc1, d)
     #
     # Output, class prediction
-    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    out = tf.add(tf.matmul(fc1, w['out']), b['out'])
+
     return out
-
-
-# 
-# <img src="http://deeplearning.stanford.edu/wiki/images/6/6c/Convolution_schematic.gif">
-# 
-# <img src="http://cs231n.github.io/assets/cnn/maxpool.jpeg">
-# 
-
-# In[5]:
 
 
 # Store layers weight & bias
@@ -114,27 +85,19 @@ biases = {
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
-
-# In[6]:
-
-
 # Construct model
-pred = conv_net(x, weights, biases, keep_prob)
+pred = conv_net(X, weights, biases, keep_prob)
 
 # Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Evaluate model
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 # Initializing the variables
 init = tf.global_variables_initializer()
-
-
-# In[12]:
-
 
 # Launch the graph
 with tf.Session() as sess:
@@ -144,12 +107,14 @@ with tf.Session() as sess:
     while step * batch_size < training_iters:
         batch_x, batch_y = mnist.train.next_batch(batch_size)
         # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, keep_prob: dropout})
+        sess.run(optimizer, feed_dict={X: batch_x, Y: batch_y, keep_prob: dropout})
         if step % display_step == 0:
             # Calculate batch loss and accuracy
-            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y, keep_prob: 1.})
+            loss, acc = sess.run([cost, accuracy], feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.})
             # Calculate accuracy for 256 mnist test images
-            t_acc = sess.run(accuracy, feed_dict={x: mnist.test.images[:256], y: mnist.test.labels[:256], keep_prob: 1.})
+            t_acc = sess.run(accuracy, feed_dict={
+                X: mnist.test.images[:256],
+                Y: mnist.test.labels[:256], keep_prob: 1.})
             print(
                 "Iter " + str(step*batch_size) +
                 ", Minibatch Loss= " + "{:.6f}".format(loss) +
@@ -159,13 +124,4 @@ with tf.Session() as sess:
         step += 1
     print("Optimization Finished!")
 
-
-# 
-# <img src="http://i.imgur.com/bSiNEq6.gif">
-# 
-
-# In[ ]:
-
-
-# usefull http://www.wildml.com/2015/11/understanding-convolutional-neural-networks-for-nlp/
-
+# useful http://www.wildml.com/2015/11/understanding-convolutional-neural-networks-for-nlp/
